e0381581ca961ec5fdd3f8ebcac81e06399b283b
tiger-tooth <2936362563@qq.com>
2018-10-11 09:38:14 +0800

    2018年10月11日

--- Aha/Aha/冒泡排序.cpp
+++ Aha/Aha/冒泡排序.cpp
@@ -38,29 +38,16 @@ struct student
 
 int main()
 {
-	struct student a[100], t;
-	int i, j, n;
+	int a[100], i, j, t, n;
+
 	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		scanf("%s %d", a[i].name, &a[i].score);
+		scanf("%d", &a[i]);
 	}
-		for (i = 1; i <= n - 1; i++)
-		{
-			for (j = 1; j <= n - 1; j++)
-			{
-				if (a[j].score < a[j + 1].score)
-				{
-					t = a[j];
-					a[j] = a[j + 1];
-					a[j + 1] = t;
-				}
-			}
-		}
-		for (i = 1; i <= n; i++)
-		{
-			printf("%s\n", a[i].name);
-		}
+
+
 
 		getchar();
 		getchar();
